67f9727140d05c489793c1810a759587da052439
austinirvine <aa1irvine@gmail.com>
2020-04-18 22:18:53 -0500

    Add Player Scripts and Collisions

---
+++ project/player/player.gd
@@ -0,0 +1,58 @@
+extends KinematicBody
+
+export var speed = 10
+export var acceleration = 5
+export var gravity = 0.98
+export var jump_power = 40
+export var mouse_sensitivity = 3
+
+onready var body = $CollisionShape
+onready var body_mesh = $CollisionShape/MeshInstance
+onready var camera = $CollisionShape/Camera
+
+var velocity = Vector3()
+
+var camera_x_rotation = 0
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		body.rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
+		var x_delta = event.relative.y * mouse_sensitivity
+		if camera_x_rotation + x_delta > -50 and camera_x_rotation + x_delta < 50:
+			camera.rotate_x(deg2rad(-x_delta))
+			camera_x_rotation += x_delta
+		print_debug("moving mouse")
+
+func _physics_process(delta):
+	var body_basis = body.get_global_transform().basis
+
+	var direction = Vector3()
+	if Input.is_action_pressed("move_forward"):
+		direction -= body_basis.z
+	elif Input.is_action_pressed("move_backward"):
+		direction += body_basis.z
+
+	if Input.is_action_pressed("move_left"):
+		direction -= body_basis.x
+	elif Input.is_action_pressed("move_right"):
+		direction += body_basis.x
+
+	direction = direction.normalized()
+
+	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
+	velocity.y -= gravity
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y += jump_power
+
+	velocity = move_and_slide(velocity, Vector3.UP)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
