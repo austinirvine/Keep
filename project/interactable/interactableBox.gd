afb874ec1892ffdf95ae085a6ce8ab899b47cf69
austinirvine <aa1irvine@gmail.com>
2020-04-20 16:14:05 -0500

    Drop Text Appears By Objects and Music

--- project/interactable/interactableBox.gd
+++ project/interactable/interactableBox.gd
@@ -1,27 +1,4 @@
 extends interactable
 
-export(NodePath) var light
-export var picked_up = false
-
-onready var grab = picked_up
-
-var playerCamera
-
 func get_interaction_text():
-	return "Grab Object" if grab else "Drop Object"
-
-func interact():
-	grab = !grab
-	if grab:
-		set_to_hold()
-
-func set_to_hold():
-	# get node to follow for grab transform
-	#playerCamera =
-	pass
-
-func _physics_process(delta):
-	if grab:
-		#set transform to that stored node
-		pass
-	pass
+	return "Grab Object" if !dragging else "Drop Object"
